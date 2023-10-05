5da4a9c6317710059ab161c9b66073ccc3356c42
git config <sandro.gagnidze.1@btu.edu.ge>
2023-10-05 17:40:55 +0400

    Update Main.java file

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,10 +2,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.testng.SoftAsserts;
 import data.CustomListener;
 import data.TestConfigClass;
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 import steps.*;
@@ -16,11 +13,12 @@ import steps.*;
 public class Main extends TestConfigClass {
     SoftAssert softAssert = new SoftAssert();
     HomePageSteps hps = new HomePageSteps();
-    RecreationPageSteps rps = new RecreationPageSteps(softAssert);
-    SushiPageSteps sushiPageSteps = new SushiPageSteps(softAssert);
-    FirstProductPageSteps fpp = new FirstProductPageSteps(softAssert);
-    RegisterPageSteps registerPageSteps = new RegisterPageSteps(softAssert);
+    RecreationPageSteps rps = new RecreationPageSteps();
+    SushiPageSteps sushiPageSteps = new SushiPageSteps();
+    FirstProductPageSteps fpp = new FirstProductPageSteps();
+    RegisterPageSteps registerPageSteps = new RegisterPageSteps();
     @BeforeMethod(groups = {"Regression1", "Regression2"})
+    @Description("Setup configuration for Main class")
     public void setUp() {
         super.setUp();
     }
@@ -28,47 +26,48 @@ public class Main extends TestConfigClass {
     @Test(groups={"Regression1"}, description = "go to 'კატეგორიები' and set price range 200 to 230")
     @Story("Page 'დასვენება' 's products  prices check")
     @Description("go to 'დასვენება' and set price range 200 to 230 and check returned products prices")
-    public void test1() {
+    @Severity(SeverityLevel.NORMAL)
+    public void testPriceRange() {
         Selenide.open("https://www.swoop.ge/");
         hps.closeCookies().goToRecreationPage();
         rps.scrollDown().closeBanner().scrollDown2().setPriceRange().waitForLoadPage().checkPriceRangeSetResult();
-        softAssert.assertAll();
     }
 
     @Test(groups={"Regression1"}, description = "try to add products to favorites on 'სუში' page")
     @Story("Add product to favorites")
     @Description("Click on 'კატეგორიები', hover on 'ჭამა', click on 'სუში', try to add first item to favorites, check if login and register page appear, check voucher not sold")
-    public void test2(){
+    @Severity(SeverityLevel.NORMAL)
+    public void testAddToFavorites(){
         Selenide.open("https://www.swoop.ge/");
         hps.clickOnCategories().hoverOnEat().clickOnSushi();
         sushiPageSteps.addToFavorites().checkIfLoginFormAppear().checkVoucher();
-        softAssert.assertAll();
     }
 
     @Test(groups={"Regression2"}, description = "Check if sorting by descending price work correctly on 'სუში' page")
     @Story("Sorting 'სუში' 's products by prices desc order")
-    @Story("Go to 'სუში' page, sort items by 'ფასით კლებადი' and check if first product's price is greater than second product's price")
-    public void test3() {
+    @Description("Go to 'სუში' page, sort items by 'ფასით კლებადი' and check if first product's price is greater than second product's price")
+    @Severity(SeverityLevel.NORMAL)
+    public void testCheckPricesOnSushiPage() {
         Selenide.open("https://www.swoop.ge/");
         hps.clickOnCategories().hoverOnEat().clickOnSushi();
         sushiPageSteps.clickOnSort().waitForSort().checkFirstAndSecondProductsPrices();
-        softAssert.assertAll();
     }
 
     @Test(groups={"Regression2"}, description = "Test if button 'გაზიარება' work on 'სუში''s page's first product")
     @Story("Check 'გაზიარება' button")
     @Description("Go to 'სუში' page, go to first product, click on 'გაზაირება' and check if new window with facebook login page appear")
-    public void test4() {
+    @Severity(SeverityLevel.NORMAL)
+    public void testCheckShareButton() {
         Selenide.open("https://www.swoop.ge/");
         hps.clickOnCategories().hoverOnEat().clickOnSushi();
         sushiPageSteps.clickFirstProduct();
         fpp.clickOnShare().checkWindow();
-        softAssert.assertAll();
     }
 
     @Test(groups={"Regression2"}, description = "Try to register new account with data from database")
     @Story("Try to register new account with data from database")
     @Description("Generate data for new user in database, get data from database and register new account based on this data without filling gender")
+    @Severity(SeverityLevel.MINOR)
     public void testDatabaseAndRegisterForm() {
         data.SqlManipulation.doInsert();
         Selenide.open("https://www.swoop.ge/");
@@ -86,9 +85,9 @@ public class Main extends TestConfigClass {
                 .checkTerms()
                 .clickOnSubmit()
                 .checkMessage();
-        softAssert.assertAll();
     }
     @AfterMethod(groups = {"Regression1", "Regression2"})
+    @Description("Close browser")
     public void tearDown() {
         super.tearDown();
     }
