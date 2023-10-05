7fea7e4515daa490f9ee22b8847ef790c9dc57be
git config <sandro.gagnidze.1@btu.edu.ge>
2023-10-05 15:04:14 +0400

    Add TestConfig.java file

---
+++ src/main/java/data/TestConfigClass.java
@@ -0,0 +1,31 @@
+package data;
+
+import com.codeborne.selenide.AssertionMode;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Configuration.fastSetValue;
+
+public class TestConfigClass {
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        Configuration.browserCapabilities = options;
+        browserSize = null;
+        baseUrl="https://www.swoop.ge/";
+        timeout=20000;
+        pageLoadTimeout=750000;
+        assertionMode= AssertionMode.SOFT;
+        holdBrowserOpen=false;
+        reopenBrowserOnFail=true;
+        savePageSource=false;
+        fastSetValue=true;
+    }
+
+    public void tearDown() {
+//        Selenide.closeWebDriver();
+        WebDriverRunner.closeWebDriver();
+    }
+}
